ac13d3a12e0ef87e6b4a7bca4b4a69727bec88d7
Michel Iseneld <84731866+iseneld@users.noreply.github.com>
2021-10-14 14:39:04 +0200

    Cleaned JS.

--- scripts/main.js
+++ scripts/main.js
@@ -5,31 +5,6 @@
 // Function: Remove outdated date.
 // Function: Only first show H:M:S when below 24h.
 
-// Set the date we're counting down to
-var countDownDate = new Date(data.date).getTime();
-
-// Update the count down every 1 second
-var x = setInterval(function() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // If the count down is finished, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.querySelector("body").innerHTML = "Yay! Today's the day!";
-  }
-}, 1000);
-
 function printHolidays(data) {
   var countDownDate = new Date(data.date).getTime();
   var li = document.createElement("li"); 
