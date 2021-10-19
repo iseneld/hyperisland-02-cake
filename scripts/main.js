a3ffb2f8afb80ccb57b3bc68bb4a3f885ac1350b
Michel Iseneld <84731866+iseneld@users.noreply.github.com>
2021-10-19 13:37:23 +0200

    Cleaned the code.

--- scripts/main.js
+++ scripts/main.js
@@ -9,35 +9,33 @@
 
 
 function createListItems(data, selected) {
-  
-    var li = document.createElement("li");
-    var h2 = document.createElement("h2");
-    var h3 = document.createElement("h3");
-    var button = document.createElement("button");
-    var div = document.createElement("div");
-    button.setAttribute("id", `${data.id}`);
-    li.setAttribute("id", `list-${data.id}`)
-    if(selected){
-      li.classList.add('favourite_selection');
-    }
-    if (data.id == 1) {
-        h2.append(data.name);
-        button.innerHTML = "fav";
-        div.append(h3);
-        li.append(h2, div);
+  var li = document.createElement("li");
+  var h2 = document.createElement("h2");
+  var h3 = document.createElement("h3");
+  var button = document.createElement("button");
+  var div = document.createElement("div");
+  button.setAttribute("id", `${data.id}`);
+  li.setAttribute("id", `list-${data.id}`);
 
-        document.querySelector("main ol").append(li);
-    }
-    else {
-        h2.append(data.name);
-        button.innerHTML = "fav";
-        div.append(h3, button);
-        li.append(h2, div);
+  if (selected) {
+    li.classList.add("favourite_selection");
+  }
 
+  if (data.id == 1) {
+    h2.append(data.name);
+    button.innerHTML = "fav";
+    div.append(h3);
+    li.append(h2, div);
 
-        document.querySelector("main ol").append(li);
-    }
-    
+    document.querySelector("main ol").append(li);
+  } else {
+    h2.append(data.name);
+    button.innerHTML = "fav";
+    div.append(h3, button);
+    li.append(h2, div);
+
+    document.querySelector("main ol").append(li);
+  }
 }
 
 function printHolidays(data) {
@@ -53,21 +51,27 @@ function printHolidays(data) {
     // Time calculations for days, hours, minutes and seconds
     var days = Math.floor(distance / (1000 * 60 * 60 * 24));
     var hours = Math.floor(
-      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
     var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
     var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
     if (distance > 86400000) {
-      document.querySelector("main ol li:nth-child(" + `${data.id}` + ") h3").innerHTML = days + " days";
+      document.querySelector(
+        "main ol li:nth-child(" + `${data.id}` + ") h3"
+      ).innerHTML = days + " days";
     } else {
       // Display the result in the element with id="demo"
-      document.querySelector("main ol li:nth-child(" + `${data.id}` + ") h3").innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
+      document.querySelector(
+        "main ol li:nth-child(" + `${data.id}` + ") h3"
+      ).innerHTML = hours + "h " + minutes + "m " + seconds + "s ";
     }
 
     // If the count down is finished, write some text
     if (distance < 0) {
       clearInterval(x);
-      document.querySelector("ol li:first-child").innerHTML = "Yay! Today's the day!";
+      document.querySelector("ol li:first-child").innerHTML =
+        "Yay! Today's the day!";
     }
   }, 1000);
 
@@ -75,72 +79,60 @@ function printHolidays(data) {
   // if(day == -1) {}
   //if (day == 0 && hour == -23 && min == -59 && sec == -59)
   //{
-        
+
   //}
 
-document.querySelector("ol li:first-child").setAttribute("class", "main__card");
-
-
+  document
+    .querySelector("ol li:first-child")
+    .setAttribute("class", "main__card");
 }
-
 
 function getHolidays() {
   fetch("../data/holidays.json")
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < 5; i++) {
-        if(!localStorage.length == 0) {
-          var selected = localStorage.getItem('favourites').includes(i + 1) }
-          createListItems(data[i], selected);
-          printHolidays(data[i]);
-
-         // Function call >>
-        
+        if (!localStorage.length == 0) {
+          var selected = localStorage.getItem("favourites").includes(i + 1);
+        }
+        createListItems(data[i], selected);
+        printHolidays(data[i]);
+        // Function call >>
       }
-      favouritesActive()
+      favouritesActive();
     });
-
 }
 
 //creating localStorage
+function favouritesActive() {
+  const buttons = document.querySelectorAll("button");
+  console.log("buttons", buttons);
+  // First check if there's favorite list already
+  // if not, set it to be an empty array
+  let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
+  console.log(favourites);
 
+  buttons.forEach((button) => {
+    button.addEventListener("click", () => {
+      //check if button have class selected
+      //if does remove class
+      //and remove from local storage
 
+      var id = this.event.target.getAttribute("id");
+      document
+        .getElementById(`list-${id}`)
+        .classList.toggle("favourite_selection");
 
-function favouritesActive() {  
-  console.log('sss')
-  const buttons = document.querySelectorAll('button')
-  console.log('buttons', buttons)
-    // First check if there's favorite list already
-    // if not, set it to be an empty array
-    let favourites = JSON.parse(localStorage.getItem('favourites')) || []
-    console.log(favourites)
-
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-       
-        //check if button have class selected
-        //if does remove class
-        //and remove from local storage
-      
-        var id = this.event.target.getAttribute('id');
-        document.getElementById(`list-${id}`).classList.toggle('favourite_selection');
-
-        if(!favourites.includes(button.id)){ 
-          favourites.push(button.id)
-          localStorage.setItem('favourites', JSON.stringify(favourites))
-          
-          console.log(localStorage.getItem('favourites'))
-          
-        }
-        var listItem = document.getElementById(`list-${id}`); 
-        console.log(listItem.classList)
-        if(listItem.classList.contains(favourite_selection)){
-          listItem.classList.toggle('favourite_selection')
-          JSON.parse(localStorage.removeItem("favourites[" + `${id + 1}` + "]")) 
-          
-        }
-      })
-      
-    })
+      if (!favourites.includes(button.id)) {
+        favourites.push(button.id);
+        localStorage.setItem("favourites", JSON.stringify(favourites));
+      }
+      var listItem = document.getElementById(`list-${id}`);
+      console.log(listItem.classList);
+      if (listItem.classList.contains(favourite_selection)) {
+        listItem.classList.toggle("favourite_selection");
+        JSON.parse(localStorage.removeItem("favourites[" + `${id + 1}` + "]"));
+      }
+    });
+  });
 }  
