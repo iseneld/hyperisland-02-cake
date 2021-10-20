948ac12be2c8d76bd9a9af10500d8bef2e4cf9ed
SofiaEmt <sofia.emteryd@gmail.com>
2021-10-20 11:01:09 +0200

    hope this works

--- scripts/main.js
+++ scripts/main.js
@@ -96,24 +96,6 @@ function getHolidays() {
     .then((response) => response.json())
     .then((data) => {
       for (let i = 0; i < 5; i++) {
-<<<<<<< HEAD
-        if(!localStorage.length == 0) {
-          var selected = localStorage.getItem('favourites').includes(i + 1) }
-          createListItems(data[i], selected);
-          printHolidays(data[i]);
-         
-          
-
-         // Function call >>
-        
-      }
-      
-
-      favouritesActive()
-    });
-
-
-=======
         if (!localStorage.length == 0) {
           var selected = localStorage.getItem("favourites").includes(i + 1);
         }
@@ -123,7 +105,6 @@ function getHolidays() {
       }
       favouritesActive();
     });
->>>>>>> 6a5c096f9b4c2d7ecbbc991e1bedd05038505854
 }
 
 //creating localStorage
@@ -137,89 +118,6 @@ function favouritesActive() {
   let favourites = JSON.parse(localStorage.getItem("favourites")) || [];
   console.log(favourites);
 
-<<<<<<< HEAD
-function favouritesActive() {  
-  console.log('sss')
-  const buttons = document.querySelectorAll('button')
-  console.log('buttons', buttons)
-    // First check if there's favorite list already
-    // if not, set it to be an empty array
-    let favourites = JSON.parse(localStorage.getItem('favourites')) || []
-    console.log(favourites)
-
-
-    buttons.forEach(button => {
-        button.addEventListener('click', () => {
-       
-        //check if button have class selected
-        //if does remove class
-        //and remove from local storage
-      
-        var id = this.event.target.getAttribute('id');
-        document.getElementById(`list-${id}`).classList.toggle('favourite_selection');
-
-        if(!favourites.includes(button.id)){ 
-          favourites.push(button.id)
-          localStorage.setItem('favourites', JSON.stringify(favourites))
-          
-          console.log(localStorage.getItem('favourites'))
-          
-        }
-        var listItem = document.getElementById(`list-${id}`); 
-        console.log(listItem.classList)
-        if(listItem.classList.contains(favourite_selection)){
-          listItem.classList.toggle('favourite_selection')
-          JSON.parse(localStorage.removeItem("favourites[" + `${id + 1}` + "]")) 
-          
-        }
-      })
-      
-    })
-}  
-
-
-
-    //first I fetch the button here:
- 
-    function buttonEnabled() {
-
-        // set variables for button and list
-    
-        var buttonElement = document.getElementsByTagName(`button`);
-        var lists = document.querySelector("li");
-    
-        // check if <li> contains class "favourite_selection", then execute class on button.
-        
-        if (lists.classList.contains("favourite_selection" )) {
-            buttonElement.setAttribute("class", "filled-button");
-        }
-        // if <li> does not contain class "favourite_selection", then remove class on button.
-        else {
-            //buttonElement.classList.remove("filled-button");
-        }
-    }
-
-    function onclickButton() {
-
-        buttonEnabled();
-    }
-
-
-    //I tried this code as well but it does same thing: buttonElement.classList.add();
-    //then when button is clicked, a class should be added:
-
-        
-    
-    //when the button is clicked again, the class should be removed:
-    //if(buttonElement.clicked == true)
-    
-
-
-    //if(document.getElementByClass("MyId").className == "hide")
-   //document.getElementById("MyId").className = "show";
-    //else
-   //document.getElementById("MyId").className = "hide";    
-=======
   buttons.forEach((button) => {
     button.addEventListener("click", () => {
       //check if button have class selected
@@ -251,4 +149,6 @@ function favouritesActive() {
     });
   });
 }
->>>>>>> 6a5c096f9b4c2d7ecbbc991e1bedd05038505854
+
+
+ 
